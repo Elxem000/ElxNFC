785a057bee0053d6e0e742a6af7530bd68e8cc95
Elxem000 <135558430+Elxem000@users.noreply.github.com>
2023-06-11 17:20:07 +0300

    Update ElxUpdate.sh

--- ElxUpdate.sh
+++ ElxUpdate.sh
@@ -1,1 +1,21 @@
+cd ..
 
+rm -fr ElxNFC
+
+clear
+
+rmdir ElxNFC
+
+clear
+
+git clone https://github.com/Elxem000/ElxNFC
+
+clear
+
+figlet "JuanE" | lolcat
+
+cd
+
+echo "ElxNFC güncelendi."
+
+echo "(⁠;ꈍ⁠ᴗ⁠ꈍ⁠)" 
